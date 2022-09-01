31c14aa2773d1f85851875c6df525bc97a626e68
Vlad7Stepanov7 <svlad4117@gmail.com>
2022-09-01 13:05:14 +0300

    тест пуш, сделал первое задание

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const btnStart = document.querySelector(`[data-start]`);
+const btnStop = document.querySelector(`[data-stop]`);
+const body = document.body;
 
+let isActive = false;
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+onBtnStopClick();
+
+btnStart.addEventListener(`click`, onBtnStartClick);
+btnStop.addEventListener(`click`, onBtnStopClick);
+
+function onBtnStartClick(e) { 
+    isActive = true;
+
+    btnStart.disabled = isActive;
+    btnStop.disabled = !isActive;
+
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        console.log(body.style.backgroundColor = getRandomHexColor());
+    }, 1000);
+    
+};
+
+function onBtnStopClick(e) {
+    clearInterval(intervalId);
+
+    isActive = false;
+
+    btnStop.disabled = !isActive;
+    btnStart.disabled = isActive;
+};
